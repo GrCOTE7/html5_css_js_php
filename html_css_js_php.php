9cc4e2bc15186e6bad2a7d0e5ae1b4a6d8d78206
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-04 10:52:34 -0800

    maj

--- html_css_js_php.php
+++ html_css_js_php.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>HTML JS PHP | Li</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    
-    <body>
-        <div id="etude_chronologie">
+  <head>
+    <meta charset="UTF-8" />
+    <title>HTML JS PHP | Li</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
 
-            <section>
-                html5
-            </section>
+  <body>
+    <div id="etude_chronologie">
 
-            <section id='myScreen' class="red">
-                <script>
-                    document.getElementById('myScreen').innerHTML = 'Je calcule...';
-                </script>
-            </section>
+      <section>
+        html5
+      </section>
 
-            <section id ="calcul">
-                <?php
-                for ($index = 0; $index < 1e6; $index++) { //1e6
-                    echo number_format($index,0,'.','&nbsp;') . ' | ';
-                }
-                ?>
-            </section>
+      <section id='myScreen' class="red">
+        <script>
+          document.getElementById('myScreen').innerHTML = 'Je calcule...';
+        </script>
+      </section>
 
-        </div>
-        <script src="html_css_js_php.js"></script>
-    </body>
+      <section id ="calcul">
+        <ul>
+          <?php
+          for ($index = 1; $index < 1e6; $index++) { //1e6
+//          for ($index = 1e5; $index < (2e5+1e2); $index++) { //1e6
+            echo '<li>';
+            echo number_format($index, 0, '.', ' ');
+//          if ($index % 8 !=0) echo ' | ';
+            echo ' ';
+            echo '</li>';
+          }
+          ?>
+        </ul>
+      </section>
+
+    </div>
+    <script src="html_css_js_php.js"></script>
+  </body>
 </html>
