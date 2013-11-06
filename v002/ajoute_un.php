0951fbeebe8d064771300381a42b15f2e02912c5
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-06 11:54:59 -0800

    maj

--- v002/ajoute_un.php
+++ v002/ajoute_un.php
@@ -2,7 +2,8 @@
 
 $get = filter_input_array(INPUT_GET);
 $n = $get['value'] + 1;
-if ($n > 1e3) {
+
+if ($n > $get['fin']) {
   $n = 'fini';
 }
 echo $n;
--- v002/jquery_ajax.js
+++ v002/jquery_ajax.js
@@ -3,37 +3,26 @@
   var deb = document.querySelector('#html'),
       rep = document.querySelector('#myScreen'),
       progressBar = document.querySelector('#maBar'),
+      list_ul = document.querySelector('#list_ul'),
       valeur = 805,
-      debut = valeur;
-
-
+      debut = valeur,
+      fin = 1e3;
+//
   deb.style.fontSize = '25px';
   rep.innerHTML = 'Calcul en Cours...';
 
-//  var contnu = document.querySelector('#aff_js');
-  var contnu = document.createElement('ul');
-  document.body.appendChild(contnu);
-  
-// $('#aff_js').html($('#aff_js').html() + '<ul>');
-//  contnu.innerHTML = '<ul>';
+  list_ul.innerHTML = '<ul id=\'myList\'>';
 
   monTimer = setTimeout(function() {
 
     while (!isNaN(valeur)) {
-      progressBar.value = ((valeur * 100 - debut) / (1000 - debut));
+      progressBar.value = ((valeur * 100 - debut) / (fin - debut));
       deb.innerHTML = ' Fini avec ' + number_format(valeur) + ' comme dernière valeur';
       valeur = ajaxli(valeur).done().responseText;
     }
-
     rep.innerHTML = 'Calcul terminé.';
     rep.style.color = 'green';
-
   }, 100);
-
-//$('#aff_js').html($('#aff_js').html() + '</ul>');
-  contnu.innerHTML = contnu.innerHTML + '</ul>';
-
-
 
   function ajaxli(value) {
 
@@ -42,13 +31,14 @@
     var request = $.ajax({
       url: "ajoute_un.php",
       data: {
-        value: value
+        value: value,
+        fin: fin
       },
       success: function(retour) {
 
 
         if (!isNaN(retour)) {
-          $('#aff_js').html($('#aff_js').html() + '<li>' + number_format(retour) + '</li>');
+          $('#myList').html($('#myList').html() + '<li>' + number_format(retour) + '</li>');
         }
 
       }
