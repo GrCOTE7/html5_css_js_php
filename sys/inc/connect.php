2354bcb5a94fa0b7119b4757c7631143aff1827e
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-07 06:17:51 -0800

    maj

--- sys/inc/connect.php
+++ sys/inc/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conf = array('host' => 'localhost', 'database' => 'concrete5', 'login' => 'root', 'password' => '');
+$conf = array('host' => 'localuhost', 'database' => 'concrete5', 'login' => 'root', 'password' => '');
 try {
   $bdd = new PDO('mysql:host=' . $conf['host'] . ';dbname=' . $conf['database'] . ';', $conf['login'], $conf['password'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
   );
@@ -9,6 +9,6 @@ try {
   $bdd->exec('SET NAMES utf8');
 } catch (PDOException $e) {
   $bdd = $conf['database'];
-  $msg_err = ' à la base de données <b>' . $bdd . '</b>.';
-  die($e->getMessage() . ' pour la connection ' . $msg_err);
+  $msg_err = ' à la Base de Données <b>' . $bdd . '</b>.';
+  die('<span class="red">'.$e->getMessage() . ' pour la connection ' . $msg_err.'</span>');
 }
