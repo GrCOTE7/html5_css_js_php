a5f6915c3703f2da06d47e80ef9479d37d5972a9
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-08 07:04:30 -0800

    maj

--- cours/closure.js
+++ cours/closure.js
@@ -4,11 +4,11 @@ var divs = document.getElementsByTagName('div'),
 console.log(divsLen);
 
 for (var i = 0; i < divsLen; i++) {
-
+  
   (function(i) {
     setTimeout(function() {
       divs[i].style.display = 'block';
     }, 500 * i); // Le temps augmentera de 200ms à chaque élément
-
+    
   })(i);
 }
