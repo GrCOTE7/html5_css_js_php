1189e09782c14f9cfeb7d2ca41d11b74789fa40f
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-08 02:54:45 -0800

    maj

--- q_forum/uuu.php
+++ q_forum/uuu.php
@@ -12,6 +12,12 @@
     <script src="../sys/lib/fct_li-0.0.1.js"></script>
 
     <link rel="stylesheet" href="../sys/css/style.css">
+     <style>
+      div {
+        /*display: none;*/
+        margin: 5px;
+      }
+    </style>
   </head>
 
   <body>
@@ -35,10 +41,10 @@
 
    
     <script>
-      function incrementer() {
-        nbclick = document.getElementById('nbre');
-        nbclick.innerHTML = parseInt(nbclick.innerHTML) + 1;
-      }
+//      function incrementer() {
+//        nbclick = document.getElementById('nbre');
+//        nbclick.innerHTML = parseInt(nbclick.innerHTML) + 1;
+//      }
 
 //      $(function() {
 //        $('#texteJQ').html('Hello world ! Ce texte est affiché par jQuery.');
@@ -50,7 +56,7 @@
       for (var i = 0; i < divsLen; i++) {
 
         setTimeout(function() {
-          divs[i].style.display = 'none';
+          divs[i].style.display = 'block';
         }, 200 * i); // Le temps augmentera de 200 ms à chaque élément
 
       }
