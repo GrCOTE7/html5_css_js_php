d76d08697996ea2a4b77925b284abc231c29e248
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-12 00:34:26 -0800

    maj

--- jquery/index.php
+++ jquery/index.php
@@ -11,23 +11,10 @@
 
 
    <style type="text/css">
-      #parent {
-        width: 250px;
-        height:250px;
-        position: absolute;
-        top: 100px;
-        left: 50px;
-        background-color: yellow;
-      }
-       
-      #enfant {
-        width: 100px;
-        height:100px;
-        position: absolute;
-        top: 100px;
-        left: 100px;
-        background-color: red;
-      }
+   
+     
+     
+     
     </style>
 
     
@@ -36,13 +23,28 @@
 
   <body>
    
-   <div id="parent">
-      Texte dans le parent
-      <div id="enfant">
-        Texte dans l'enfant
-      </div>
-    </div>
-    <span id="resultat"></span>    
+ <h2 id="un">Lorem ipsum</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <hr>
+ 
+    <h2 id="deux">Lorem ipsum suite</h2>
+    <p>
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <hr>
+ 
+    <h2 id="trois">Liste à puces</h2>
+    <ul>
+      <li>Premier élément</li>
+      <li>Deuxième élément</li>
+      <li>Troisième élément</li>
+      <li>Quatrième élément</li>
+    </ul>      
+        
 
 
     <script src="../sys/lib/jquery-2.0.3.min.js"></script>
--- jquery/jq_li.js
+++ jquery/jq_li.js
@@ -1,24 +1,12 @@
 $(function() {
 
-  var posparent = $('#parent').offset();
-  var posenfant = $('#enfant').offset();
-  var text1 = 'Parent : x=' + posparent.left + ', y=' + posparent.top + ' Enfant : x=' + posenfant.left + ', y=' + posenfant.top;
+ 
+$('h2').prepend('*** ');
+ 
+ 
+$('#trois').before('<hr>');
 
-  var posparent = $('#parent').position();
-  var posenfant = $('#enfant').position();
-
-  var text2 = ('Parent : x=' + posparent.left + ', y=' + posparent.top + ' Enfant : x=' + posenfant.left + ', y=' + posenfant.top);
-
-  $('span').text(text1+'\n'+text2);
-
-
-var posenfant = $('#enfant').offset();
-posenfant.top = 150;
-posenfant.left = 120;
-$('#enfant').offset(posenfant);
-
-
-
+$('hr').after('<br><br>');
 
 });
 
