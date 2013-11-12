9afe725bcd545ac41ce912ac9f476b922ea164d5
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-11 20:25:03 -0800

    maj

---
+++ jquery/jq_li.js
@@ -0,0 +1,40 @@
+var divs = document.getElementsByTagName('div'),
+    rep = document.querySelector('#rep');
+divsLen = divs.length;
+console.log(divsLen);
+
+$(function() {
+  $('#texteJQ').html('Hello world ! Ce texte est affiché par jQuery.');
+});
+
+
+for (var i = 0; i < divsLen; i++) {
+
+  (function(i) {
+    setTimeout(function() {
+      divs[i].style.display = 'block';
+    }, 500 * i); // Le temps augmentera de 200ms à chaque élément
+
+  })(i);
+}
+
+
+
+var job = 'AGENT',
+    b = job.substring(0, 2).toLowerCase() + job.substring(2);
+
+
+
+
+
+
+
+
+rep.innerHTML = b;
+rep.innerHTML += '<hr>';
+
+
+
+
+
+
