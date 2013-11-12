02027586a7468dc5561606425268fdfa6558a339
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-12 12:23:56 -0800

    maj

--- jquery/uuu.php
+++ jquery/uuu.php
@@ -2,8 +2,12 @@
 $title = 'uuu';
 include_once('./inc/head_li.php');
 ?>
+<span>BlaBla</span>
 
-BLBLB
-
-<?php
+  
+  <?php
 include_once './inc_lib.php';
+?>
+<script src="jq_li.js"></script>
+<?php
+include_once './inc/footer_li.php';
