12d5627aa979dac46475556d39ef557b79325ca9
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-13 10:58:01 -0800

    maj

--- jquery/index.php
+++ jquery/index.php
@@ -5,6 +5,16 @@ include_once('./inc/head_li.php');
 ?>
 
 
+<style>
+  div {
+    background:yellow; 
+    font-weight:bold; 
+    cursor:pointer; 
+    padding:8px; 
+  }
+</style>
+
+
 
 <span id="resultat"></span>
 
@@ -17,17 +27,17 @@ Cliquez sur l'image<br />
 
 
 <h4>Page Cible :</h4>
-<iframe id="page_cible" name="frame" src="http://localhost/js_php/jquery/page_cible.html" style="height: 100px; width: 95%; border:1x solid red; margin: 0 auto;">
+<iframe id="page_cible" name="frame" src="http://boosteur.com" style="height: 100px; width: 95%; border:1x solid red; margin: 0 auto;">
 </iframe>
 
 <div><br /></div>
 
 <?php
-$i=0;
-do{
-  echo $i.' ';
-  
-} while  (++$i<100);
+
+$i = 0;
+do {
+  echo $i . ' ';
+} while ($i++ < 100);
 echo '<hr>Fini';
 ?>
 
