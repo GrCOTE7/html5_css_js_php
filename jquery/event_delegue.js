f05a0c49db40fb3b1c4c6511ccd337b1d84669c7
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-13 11:50:21 -0800

    maj

--- jquery/event_delegue.js
+++ jquery/event_delegue.js
@@ -1,14 +1,9 @@
 $(function() {
 
-Notifier.c7('Go !');
+  Notifier.c7('Go !');
 
-  $('.event').on('click', '.event', function(){
+  $('.event').on('click', '.event', function() {
     $(this).after('<div class="event">Ce &lt;div&gt; a les mêmes caractéristiques que son parent</div>');
   });
-
-
-
-
-
 
 });
---
+++ jquery/event_delegue.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Event_JQuery';
+include_once('./inc/head_li.php');
+?>
+<div id="master" class='event'>
+  <div class='event'>Cliquez pour insérer un autre &ltdiv&gt</div>
+</div>
+<!--<script src="jquery.js"></script>-->
+<?php
+include_once './inc_lib.php';
+?>
+<script src="event_delegue.js"></script>
+
+<?php
+include_once './inc/footer_li.php';
