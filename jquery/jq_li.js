6f1e224550b2a46474ecac4802709b4a625404a4
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-14 06:10:03 -0800

    maj

--- jquery/jq_li.js
+++ jquery/jq_li.js
@@ -7,22 +7,21 @@ $(function() {
 
   var myLogo = $('#Animate_Jq img');
   myLogo.css('border', '1px solid red');
-  myLogo.animate({'border-width': '100'}, 1500)
-      .animate({'width': '-=10%', 'height': '-=10%'}, 1000)
-      .animate({'border-width': '2'}, 1500)
-      .animate({'margin-top': '300'}, 1500)
-      .animate({'border-width': '0'}, 1000)
-      .animate({'margin-left': '380', 'margin-top': '0'}, 1500);
+  myLogo.animate({borderWidth: '100'}, 1500)
+      .animate({width: '-=10%', 'height': '-=10%'}, 1000)
+      .animate({borderWidth: '2'}, 1500)
+      .animate({marginTop: '300'}, 1500)
+      .animate({borderWidth: '0'}, 1000)
+      .animate({marginLeft: '380', 'margin-top': '0'}, 1500);
 
 
   var monTimer = setInterval(function() {
-    var n = $('#Animate_Jq img').queue();
-    myJq.html('Nombre d\'animations dans la file d\'attente : ' + n.length);
+    myJq.html('Nombre d\'animations dans la file d\'attente : ' + myLogo.queue().length);
   }, 100);
 
   setTimeout(function() {
     clearInterval(monTimer);
-  },15000);
+  }, 15000);
 
 
 });
