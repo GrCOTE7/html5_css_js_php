43124b4ed73ce63605418b464fbbfcf0eba96337
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-14 11:16:35 -0800

    maj

--- jquery/newEmptyPHP.php
+++ jquery/newEmptyPHP.php
@@ -1,26 +1,22 @@
-<style type="text/css">
-  #balle {
-    width: 100px;
-    height: 100px;
-    background-color: red;
-    border: black 2px solid;
-    border-radius: 100px;
-    position: relative;
-  }
-</style>
+<pre><p>Recherches et remplacements de textes
 
-<div id="balle"></div>
+Il est parfois nécessaire de sélectionner le ou les éléments qui contiennent un mot ou un texte particulier. Pour cela, vous utiliserez le pseudo-sélecteur :contains. Par exemple, pour sélectionner toutes les balises < div > qui contiennent le mot « rouge », vous utiliserez le sélecteur suivant :
 
+1$('div:contains("rouge")')
+Vous pourriez par exemple modifier la couleur d'arrière-plan des < div > qui contiennent le mot « rouge » en utilisant cette instruction :
+
+1$('div:contains("rouge")').css('background-color', 'red');
+Il est également possible de remplacer un texte par un autre ou un élément par un autre en utilisant la méthode replaceWith(), dont voici la syntaxe :
+
+1$('sel').replaceWith('contenu');
+… où sel est un sélecteur jQuery et contenu une chaîne HTML, un élément du DOM ou un objet jQuery qui remplacera le ou les éléments sélectionnés. Regardez le code suivant, tout deviendra limpide.
+</p></pre>
 <script src="jquery.js"></script>
 <script>
-  
   $(function() {
-    function bis() {
-      $('#balle').animate({left: '+=200'}, 10000)
-                 .animate({top: '+=200'}, 10000)
-                 .animate({left: '-=200'}, 1e4)
-                 .animate({top: '-=200'}, 1e4, bis);
-    };
-    bis();
+  
+  $('p:contains("rouge")').css('background-color', 'red');
+  $('p:contains("rouge")').replaceWith('okkk');
+  
   });
 </script>
