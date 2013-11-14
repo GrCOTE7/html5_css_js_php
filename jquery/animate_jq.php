7416c4dda8e94e979c12b150327f2269885e30f6
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-14 12:49:52 -0800

    maj

--- jquery/animate_jq.php
+++ jquery/animate_jq.php
@@ -1,10 +1,11 @@
 <?php
-$title = 'Array_Jq';
+$title = 'Animate_Jq';
 include_once('./inc/head_li.php');
 ?>
 
 <div id ='myJq'></div>
 
+<img src="img/c7.png" width="189" height="117" alt="c7"/>
 
 
 
@@ -40,7 +41,7 @@ include_once('./inc/head_li.php');
 <?php
 include_once './inc_lib.php';
 ?>
-<script src="jq_li.js"></script>
+<script src="animate_jq.js"></script>
 
 <?php
 include_once './inc/footer_li.php';
--- jquery/newEmptyPHP.php
+++ jquery/newEmptyPHP.php
@@ -1,22 +1,22 @@
-<pre><p>Recherches et remplacements de textes
+<span id="status">Cliquez sur un des éléments du formulaire</span><br /><br />
+<form>
+  <input type="text" id="zone1"><br />
+  <input type="text" id="zone2"><br />
+  <input type="text" id="zone3"><br />
+  <textarea id="zone4"></textarea>
+</form>    
 
-Il est parfois nécessaire de sélectionner le ou les éléments qui contiennent un mot ou un texte particulier. Pour cela, vous utiliserez le pseudo-sélecteur :contains. Par exemple, pour sélectionner toutes les balises < div > qui contiennent le mot « rouge », vous utiliserez le sélecteur suivant :
-
-1$('div:contains("rouge")')
-Vous pourriez par exemple modifier la couleur d'arrière-plan des < div > qui contiennent le mot « rouge » en utilisant cette instruction :
-
-1$('div:contains("rouge")').css('background-color', 'red');
-Il est également possible de remplacer un texte par un autre ou un élément par un autre en utilisant la méthode replaceWith(), dont voici la syntaxe :
-
-1$('sel').replaceWith('contenu');
-… où sel est un sélecteur jQuery et contenu une chaîne HTML, un élément du DOM ou un objet jQuery qui remplacera le ou les éléments sélectionnés. Regardez le code suivant, tout deviendra limpide.
-</p></pre>
 <script src="jquery.js"></script>
 <script>
   $(function() {
-  
-  $('p:contains("rouge")').css('background-color', 'red');
-  $('p:contains("rouge")').replaceWith('okkk');
-  
+    var leFocus;  
+    $('input, textarea').focus( function() { 
+      leFocus = '#' + $(this).attr('id');        
+      $(leFocus).css('background-color', '#afc');
+    }); 
+    $('input, textarea').blur( function() { 
+      leFocus = '#' + $(this).attr('id');        
+      $(leFocus).css('background-color', '#fff');
+    }); 
   });
 </script>
