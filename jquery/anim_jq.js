147b5a4287d15aaf2e796a3b75b9ab505dbb5db9
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-14 05:09:38 -0800

    maj

--- jquery/anim_jq.js
+++ jquery/anim_jq.js
@@ -1,18 +1,18 @@
 $(function() {
 
   Notifier.c7('Go !');
-  
-   
+
+
   $('body').css('display', 'none');
   $('body').delay(3000).fadeTo(2000, 0.5);
   $('body').fadeTo(1000, .1);
   $('body').fadeTo(5000, 1);
 
-$('#message').fadeIn('slow').delay(10000).fadeOut(3000);
+  $('#message').fadeIn('slow').delay(10000).fadeOut(3000);
 
-  setTimeout(function(){
-$('span#message').remove();
-  },25000);
+  setTimeout(function() {
+    $('span#message').remove();
+  }, 25000);
 
 
 
@@ -27,7 +27,7 @@ $('span#message').remove();
 
 //  jQuery.fx.off = false;
 
-  var myTest = jQuery.fx.off;
-  $('#myJq').html('XXX: ' + myTest + '<hr>');
+//  var myTest = jQuery.fx.off;
+//  $('#myJq').html('XXX: ' + myTest + '<hr>');
 });
 
---
+++ jquery/anim_jq.php
@@ -0,0 +1,77 @@
+<?php
+$title = 'Anim_JQuery';
+include_once('./inc/head_li.php');
+
+
+
+?>
+
+
+
+
+
+<div id ='myJq'></div>
+
+<span id="message"><mark> Ce texte sera affiché pendant deux secondes </mark></span>
+
+<div id="myImg">
+<img src="img/bon_rd.png" width="46" height="46" alt="bon_rd" />
+</div>
+
+
+
+
+<div id='myCpt'>
+
+<?php
+for ($index = 1; $index <= 1e2; $index+=3) {
+  echo '<span class=' . ( ($index % 10) ? 'blue' : 'red' ) . '> ' . $index . '</span>';
+}
+
+?>
+</div>
+<hr>
+<div class='stopAnim' id ='stopAnim'>Stop Anim</div>
+<div class='execAnim' id = 'execAnim'>Exec Anim</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!--<script src="jquery.js"></script>-->
+<?php
+include_once './inc_lib.php';
+?>
+<script src="anim_jq.js"></script>
+
+<?php
+include_once './inc/footer_li.php';
