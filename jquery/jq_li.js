50f39725903dd1e1cd3bab77c80f24b49fbaaffe
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-15 18:02:04 -0800

    maj

--- jquery/jq_li.js
+++ jquery/jq_li.js
@@ -1,25 +1,34 @@
 $(function() {
 
-  Notifier.c7('Go 21 !');
+  Notifier.c7('Go !');
   var myJq = $('#myJq'), output = '';
 
-//  myJq.html('Oki');
 
-
+  var monTab = [];
   for (i = 1; i < 11; i++) {
+    monTab.push(i);
+  }
+  
+  
+  output = '<u>Mon tableau <b>d\'origine</b> :</u><br>' + monTab + '<hr>';
+
+
+  var sortieTab = '';
+  monTab.forEach(function(value) {
+    sortieTab += '<span class="' + ((value % 2) ? 'blue' : 'red') + '">' + value + '</span> ';
+  });
+  output += '<u>Mon tableau <b>formaté</b> :</u><br>' + sortieTab;
+
+
+
+
+
+  output += '<hr><u>Boucle de base :</u><br>';
+  for (i = 1; i < 101; i++) {
     output += '<span class="' + ((i % 2) ? 'blue' : 'red') + '">' + i + '</span> ';
   }
-
-
-
-
-
-
-  output += '<hr>Boucle de base:<br>';
-  for (i = 7; i < 11; i++) {
-    output += '<span class="' + ((i % 2) ? 'blue' : 'red') + '">' + i + '</span> ';
-  }
+  myJq.css('display','none');
   myJq.html(output);
+  myJq.fadeIn(10000);
 
 });
-
