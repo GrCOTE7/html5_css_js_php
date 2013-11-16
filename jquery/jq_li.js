87138556cdc38f0ddb6d7fdc9b9482cd825dc665
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-15 17:57:57 -0800

    maj

--- jquery/jq_li.js
+++ jquery/jq_li.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  Notifier.c7('Go !');
+  Notifier.c7('Go 21 !');
   var myJq = $('#myJq'), output = '';
 
 //  myJq.html('Oki');
@@ -16,7 +16,7 @@ $(function() {
 
 
   output += '<hr>Boucle de base:<br>';
-  for (i = 1; i < 11; i++) {
+  for (i = 7; i < 11; i++) {
     output += '<span class="' + ((i % 2) ? 'blue' : 'red') + '">' + i + '</span> ';
   }
   myJq.html(output);
