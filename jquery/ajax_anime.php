a1b7ff38a9e7338d03382fe9fb3f700ee2463e17
GrCOTE7 <GrCOTE7@GMail.com>
2013-11-16 16:41:52 -0800

    maj

--- jquery/ajax_anime.php
+++ jquery/ajax_anime.php
@@ -14,6 +14,22 @@
   <body>
 
     <div id='exo'>
+      <div id='lionel'></div>
+
+      <section id='fondu'>
+
+        <div id='nb0' class='theNb'>0</div>
+        <div id='nb1' class='theNb'>1</div>
+        <div id='nb2' class='theNb'>2</div>
+        <div id='nb3' class='theNb'>3</div>
+        <div id='nb4' class='theNb'>4</div>
+        <div id='nb5' class='theNb'>5</div>
+        <div id='nb6' class='theNb'>6</div>
+        <div id='nb7' class='theNb'>7</div>
+        <div id='nb8' class='theNb'>8</div>
+        <div id='nb9' class='theNb'>9</div>
+
+      </section>
 
       <section id='buttons'>
 
@@ -22,9 +38,7 @@
 
       </section>
 
-
       <section id='lesDiv'>
-
 
         <div id="premier" class='ajax'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -56,84 +70,66 @@
       <script>
 
 
-//              $('#premier').fadeOut(4000);
-
-
-//        $('#premier').animate({
-//          color: 'red'
-//        }, 5000);
-
-        $('#majPremier').fadeTo(3000, 0);
-        $('#premier').fadeTo(2000, 0).delay(3000);
-
-        setTimeout(function() {
-          $('#premier').load('maj1.html', function() {
-            $('#premier').fadeTo(2000, 1).delay(3000);
-            Notifier.success('Par AJAX', 'La première zone a été mise à jour');
-          });
-        }, 5000);
-//        
-//            .animate({
-//              opacity: 0,
-//              queue: true
-//            }, 1000)
-//            .animate({
-//              height:'0px',
-//              color:'red',
-//              opacity: 1,
-//              queue: true
-//            }, 1000);
-
         $(function() {
+
+
+          $('#premier').fadeTo(1000, 0).delay(1000);
+          $('#premier').fadeTo(1000, 1).delay(1000);
+          $('#premier').fadeTo(1000, 0).delay(1000);
+          $('#premier').fadeTo(1000, 1).delay(1000);
+
+
+          clickPossible = 0;
+
+          var i = 0;
+//          affiche(i);
+
+          function affiche(i) {
+//        while (i < 10) {
+
+            var monTimer = setInterval(function() {
+
+              $('#nb' + i).fadeOut(700);
+              $('#nb' + (i + 1)).fadeIn(700);
+              i++;
+              if (i > 8) {
+                clearInterval(monTimer);
+                $('#nb9').fadeOut(2000);
+
+              }
+            }, 1000);
+          }
+
+
+
+
           Notifier.c7('Clic sur un bouton ou le Logo !');
           $('#majPremier').click(function() {
-            $('#majPremier').fadeOut(4000);
+
+            $('#premier').fadeTo(2000, 0);
+            $('#majPremier').fadeTo(2000, 0);
+
             setTimeout(function() {
-              $('#premier').css('display', 'none');
               $('#premier').load('maj1.html', function() {
                 Notifier.success('Par AJAX', 'La première zone a été mise à jour');
               });
             }, 5000);
-
           });
-//          $('#majPremier').fadeOut(7000);
-//          $('#majPremier').css('display', 'none');
-//          
 
-          //$('#exo img').attr('src', 'img/chat.jpg').fadeIn(3000);
-          //$('#deuxiemeChat').attr('src', './img/c7.png');
           $('.deuxiemeImg').fadeIn(3000);
-
-
-          //$('#exo img').delay(2000);
-          //clic
-          //        .delay(5000).fadeIn(2000);
           $('.deuxiemeImg').fadeOut(5000);
           $('.deuxiemeLogoC7').delay(4000).fadeIn(5000);
 
-          var clickPossible = 1;
+          clickPossible = 1;
 
-          //        $('#deuxiemeLogoC7').delay(2000).attr('src', 'img/c7.png').fadeIn(200);
-
-          /*
-           $('#majDeuxieme').click(function() {
-           $('#deuxieme').load('maj2.html', function() {
-           Notifier.success('Par AJAX', 'La deuxième zone est mise à jour');
-           $('.deuxiemeImg').fadeIn(3000);
-           $('#deuxiemeLogoC7').fadeOut(3000);
-           setTimeout(function() {
-           $('.deuxiemeImg').fadeOut(7000);
-           $('#deuxiemeLogoC7').fadeIn(7000);
-           }, 3000);
-           setTimeout(function() {
-           Notifier.success('( Juste en jQ )', 'La deuxième zone a été ré-initialisée');
-           }, 5000);
-           });
-           });
-           */
+          $('#majDeuxieme').addClass('Ok').removeClass('notOk');
           var eventClicOnBytnOrChat = function() {
             $('#deuxieme').css('border', '4px solid red');
-            if (clickPossible) {
+            if (clickPossible == 1) {
+              $('#majDeuxieme').addClass('notOk').removeClass('Ok');
+              $('div.ajax:nth-of-type(2)').css('border-radius', '70px')
+                  .css('backgroundColor', '#000');
+
               clickPossible = 0;
               $('#deuxieme').load('maj2.html', function() {
                 Notifier.success('Par AJAX', 'La deuxième zone est mise à jour');
@@ -144,22 +140,23 @@
                   $('.deuxiemeLogoC7').fadeIn(3000);
                 }, 3000);
                 setTimeout(function() {
-                  Notifier.success('( Juste en jQ )', 'La deuxième zone a été ré-initialisée');
-                  $('.deuxiemeLogoC7').click(eventClicOnBytnOrChat);
-                  $('#majDeuxieme').click(eventClicOnBytnOrChat);
-                  clickPossible = 1;
+                  Notifier.success('( Juste en jQ ) - Clic à nouveau possible...', 'La deuxième zone a été ré-initialisée');
                   $('#deuxieme').css('border', '4px solid greenyellow');
+                  clickPossible = 1;
+                  $('#majDeuxieme').addClass('Ok').removeClass('notOk');
+
+                  $('div.ajax:nth-of-type(2)').css('border-radius', '14px')
+                      .css('backgroundColor', 'cornsilk');
+
                 }, 5000);
               });
             }
             else {
               Notifier.error('Merci d\'attendre la fin de l\'affichage');
             }
-            ;
           };
           $('#deuxieme').click(eventClicOnBytnOrChat);
           $('#majDeuxieme').click(eventClicOnBytnOrChat);
-
         });
 
       </script>
